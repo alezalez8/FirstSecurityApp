1e83cb95db14a144f47ff40c1f348cd65f506a1f
alezalez8 <5446943@ukr.net>
2022-07-05 18:37:22 +0300

    begin lesson 79

--- src/main/java/org/shunin/firstsecurityapp/config/SecurityConfig.java
+++ src/main/java/org/shunin/firstsecurityapp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.shunin.firstsecurityapp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,6 +19,13 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     @Autowired
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
+    }
+
+    // add lesson 79
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // конфигурируем сам Spring Security
+        // конфигурируем авторизацию
     }
 
 
---
+++ src/main/java/org/shunin/firstsecurityapp/controllers/AuthController.java
@@ -0,0 +1,22 @@
+package org.shunin.firstsecurityapp.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "/auth/login";
+
+    }
+
+   /* @PostMapping()
+    public String */
+}
