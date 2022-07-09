145220fae4aa3880db9cfb719aa2f8e653a2daf5
alezalez8 <5446943@ukr.net>
2022-07-09 13:43:52 +0300

    end lesson 81 with my method PersonService

--- src/main/java/org/shunin/firstsecurityapp/config/SecurityConfig.java
+++ src/main/java/org/shunin/firstsecurityapp/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -45,11 +46,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // Настраивает аутентификацию
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);
+        auth.userDetailsService(personDetailsService)
+                .passwordEncoder(getPasswordEncoder());  // то, что ввел в окне логина, тоже прогоняется через BCrypt
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
+
+        // return NoOpPasswordEncoder.getInstance();
+
     }
 }
--- src/main/java/org/shunin/firstsecurityapp/services/RegistrationService.java
+++ src/main/java/org/shunin/firstsecurityapp/services/RegistrationService.java
@@ -3,6 +3,7 @@ package org.shunin.firstsecurityapp.services;
 import org.shunin.firstsecurityapp.models.Person;
 import org.shunin.firstsecurityapp.repositories.PeopleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,14 +11,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class RegistrationService {
 
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getUsername()));
         peopleRepository.save(person);
     }
 
